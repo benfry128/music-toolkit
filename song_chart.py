d97e32fbecbd7f8b71413363daabae3de3d694f8
benfry128 <benfry128@gmail.com>
2025-12-10 00:15:17 -0500

    Update song_chart.py to work with updated db structure from last year, as well as a new view which is nicer to use Also reset it so that it does each month

--- song_chart.py
+++ song_chart.py
@@ -5,20 +5,19 @@ sp = utils.spotipy_setup()
 
 (db, cursor) = utils.db_setup()
 
-for i in range(1, 12):
-    start_time = datetime(2024, 1, 1)
-    end_time = datetime(2025, 1, 1)
+year = 2025
 
-    start_timestamp = start_time.timestamp()
-    end_timestamp = end_time.timestamp()
+for i in range(8, 9):
+    start_time = datetime(year, i, 1)
+    end_time = datetime(year + int(i == 12), i * int(i != 12) + 1, 1)
+    if i == 12:
+        end_time = datetime(year + 1, 1, 1)
 
     cursor.execute('''
-        select albums_url.image_url from scrobbles
-            join tracks on tracks.id = scrobbles.track_id
-            join albums_url on albums_url.id = tracks.album_id
-        where utc > %s and utc < %s
-        group by albums_url.id order by count(*) desc limit 36;
-        ''', [start_timestamp, end_timestamp])
+        select image_url from all_scrobbles
+        where scrobble_time >= %s and scrobble_time < %s
+        group by image_url order by sum(runtime) desc limit 12;
+        ''', [start_time, end_time])
 
     album_info = [row[0] for row in cursor.fetchall()]
 
