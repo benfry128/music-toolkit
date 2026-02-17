5da3ac129c7832eae3d95c38d181ebe44616d5f2
benfry128 <benfry128@gmail.com>
2026-02-16 22:35:40 -0500

    Rename d to db_sanitize_metadata for clarity
    Also add code that goes through possible duplicate/deluxe versions of albums and asks to merge them, if not then they go to the metadata file since we know they can't be merged

--- db_sanitize.py
+++ db_sanitize.py
@@ -3,6 +3,8 @@ import json
 
 VERBOSE = False
 
+sp = utils.spotipy_setup()
+
 (db, cursor) = utils.db_setup()
 
 dupe_checks = ['''SELECT utc FROM
@@ -35,16 +37,16 @@ for dupe_check in dupe_checks:
 with open('db_sanitize_metadata.json', encoding='utf-8') as f:
     text = f.read()
 
-d = json.loads(text)
+db_sanitize_metadata = json.loads(text)
 
-unrelated_track_ids = d['non_dupe_track_ids']
+non_dupe_track_ids = db_sanitize_metadata['non_dupe_track_ids']
 
 print('Checking for duplicate tracks...')
 cursor.execute('SELECT track, artist FROM all_urls GROUP BY track, artist HAVING COUNT(*) > 1;')
 for track, artist in cursor.fetchall():
     cursor.execute('SELECT track_id, album FROM all_urls WHERE track = %s AND artist = %s', (track, artist))
     dupe_records = cursor.fetchall()
-    if all([record[0] in unrelated_track_ids for record in dupe_records]):
+    if all([record[0] in non_dupe_track_ids for record in dupe_records]):
         if VERBOSE:
             print(f'Skipping {track} by {artist}')
         continue
@@ -62,10 +64,39 @@ for track, artist in cursor.fetchall():
         continue
 
     for track_id, _ in dupe_records:
-        if track_id not in unrelated_track_ids:
-            unrelated_track_ids.append(track_id)
+        if track_id not in non_dupe_track_ids:
+            non_dupe_track_ids.append(track_id)
 
-d['non_dupe_track_ids'] = unrelated_track_ids
+db_sanitize_metadata['non_dupe_track_ids'] = non_dupe_track_ids
+
+print('Checking for duplicate/deluxe albums...')
+cursor.execute('''select a1.id, a2.id, max(ar1.name), a1.name,  a2.name, a1.uri, a2.uri from albums a1
+	join tracks t1 on t1.album_id = a1.id join tracks_artists ta1 on t1.id = ta1.track_id join artists ar1 on ar1.id = ta1.artist_id and ta1.main
+	join albums a2 on (a1.name like concat(a2.name, '%') or a2.name like concat(a1.name, '%')) and a1.id > a2.id
+	join tracks t2 on t2.album_id = a2.id join tracks_artists ta2 on t2.id = ta2.track_id join artists ar2 on ar2.id = ta2.artist_id and ta2.main
+    group by a1.id, a1.name, a2.id, a2.name, a1.uri, a2.uri
+    having max(ar1.name) = max(ar2.name)
+    order by a1.name;''')
+
+updated_non_dupe_album_ids = db_sanitize_metadata['non_dupe_album_ids']
+
+for album1_id, album2_id, artist, album1_name, album2_name, album1_uri, album2_uri in cursor.fetchall():
+    if album1_id in db_sanitize_metadata['non_dupe_album_ids'] and album2_id in db_sanitize_metadata['non_dupe_album_ids']:
+        continue
+    print(f'\nPossible duplicate albums from {artist}: {album1_name} and {album2_name}.\nUris are {album1_uri} and {album2_uri}')
+
+    should_merge = input('Should these be merged?')
+    if not should_merge:
+        if album1_id not in updated_non_dupe_album_ids:
+            updated_non_dupe_album_ids.append(album1_id)
+        if album2_id not in updated_non_dupe_album_ids:
+            updated_non_dupe_album_ids.append(album2_id)
+        continue
+
+    utils.merge_albums([album1_id, album2_id], sp, db, cursor)
+
+updated_non_dupe_album_ids.sort()
+db_sanitize_metadata['non_dupe_album_ids'] = updated_non_dupe_album_ids
 
 with open("db_sanitize_metadata.json", 'w', encoding='utf-8') as f:
-    f.write(json.dumps(d, indent=4))
+    f.write(json.dumps(db_sanitize_metadata, indent=4))
