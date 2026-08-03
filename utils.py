1f1f7f459dc00647e1d957b704bf3c176f008958
benfry128 <benfry128@gmail.com>
2026-08-03 17:47:16 -0400

    Update redirect url because of spotify API changes

--- utils.py
+++ utils.py
@@ -40,7 +40,7 @@ def spotipy_setup():
     scope = 'ugc-image-upload user-read-playback-state user-modify-playback-state user-read-currently-playing playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public  user-follow-modify user-follow-read user-read-playback-position user-top-read user-read-recently-played user-library-modify user-library-read user-read-email user-read-private'
     return spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
                                                      client_secret=SPOTIFY_CLIENT_SECRET,
-                                                     redirect_uri="http://localhost:1234",
+                                                     redirect_uri="http://127.0.0.1:1234",
                                                      scope=scope),
                            requests_timeout=10,
                            retries=1)
